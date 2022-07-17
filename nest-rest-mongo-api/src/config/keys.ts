76e23c76cf388120202c69f6cb1bb8d676049d4c
affanmustafa <affanmustafa2297@gmail.com>
2022-07-17 21:57:33 +0500

    Final Commit\

--- nest-rest-mongo-api/src/config/keys.ts
+++ nest-rest-mongo-api/src/config/keys.ts
@@ -1,4 +1,3 @@
 export default {
-  mongoURI:
-    'mongodb+srv://mongouseraffan:Start12345@cluster0.pye5e.mongodb.net/?retryWrites=true&w=majority',
+  mongoURI: 'mongoDBconnectionString', //Insert MongoDB Connection String here
 };
